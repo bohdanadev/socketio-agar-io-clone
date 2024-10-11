1a04b206bab1d8be424e64afdfb7201117c66441
Bohdana K <bohdana.dev@gmail.com>
2024-10-11 16:29:54 +0300

    Getting the DOM setup

--- public/ui.js
+++ public/ui.js
@@ -1,7 +1,35 @@
 let wHeight = window.innerHeight;
 let wWidth = window.innerWidth;
 
+let player = {};
+
 let canvas = document.querySelector('#the-canvas');
 let context = canvas.getContext('2d');
 canvas.width = wWidth;
 canvas.height = wHeight;
+
+const loginModal = new bootstrap.Modal(document.querySelector('#loginModal'));
+const spawnModal = new bootstrap.Modal(document.querySelector('#spawnModal'));
+
+window.addEventListener('load', () => {
+  loginModal.show();
+});
+
+document.querySelector('.name-form').addEventListener('submit', (e) => {
+  e.preventDefault();
+  player.name = document.querySelector('#name-input').value;
+  document.querySelector('.player-name').innerHTML = player.name;
+  loginModal.hide();
+  spawnModal.show();
+});
+
+document.querySelector('.start-game').addEventListener('click', () => {
+  spawnModal.hide();
+  const elArray = document.querySelectorAll('.hiddenOnStart');
+
+  elArray.forEach((el) => {
+    el.removeAttribute('hidden');
+  });
+
+  init();
+});
