a825c7da56093bffe3009f0b9f6e69747fb9cf79
Bohdana K <bohdana.dev@gmail.com>
2024-10-13 01:20:15 +0300

    Create player classes

--- socket/socketMain.js
+++ socket/socketMain.js
@@ -1,13 +1,29 @@
 const io = require('../server').io;
 const app = require('../server').app;
 
+const Player = require('./classes/Player');
+const PlayerConfig = require('./classes/PlayerConfig');
+const PlayerData = require('./classes/PlayerData');
 const Orb = require('./classes/Orb');
 
 const orbs = [];
+const settings = {
+  defaultNumberOfOrbs: 2000,
+  defaultSpeed: 6,
+  defaultSize: 6,
+  defaultZoom: 1.5,
+  worldWidth: 2000,
+  worldHeight: 2000,
+  defaultGenericOrbSize: 5,
+};
 
 initGame();
 
 io.on('connect', (socket) => {
+  const playerName = 'Player';
+  const playerConfig = new PlayerConfig(settings);
+  const playerData = new PlayerData(playerName, settings);
+  player = new Player(socket.id, playerConfig, playerData);
   socket.emit('init', {
     orbs,
   });
@@ -15,6 +31,6 @@ io.on('connect', (socket) => {
 
 function initGame() {
   for (let i = 0; i < 500; i++) {
-    ResizeObserverSize.push(new Orb());
+    orbs.push(new Orb(settings));
   }
 }
